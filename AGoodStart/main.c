8ad0c3112b4c8a4cd91a8c35eed9c07973e4bc98
sravaninv <vijayasravanin@gmail.com>
2017-03-06 11:57:46 -0600

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Sravani on 2/13/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    //Print the begining of the novel
+    printf("It was best of times.\n");
+    printf("It was worst of times.\n");
+    /* Is that actually any good?
+     may be it needs a rewrite*/
+    return 0;
+    
+    }
